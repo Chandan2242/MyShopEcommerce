492f12310359e99ec1f1aff517ef47ad106f2b1b
chandan Pragapati <chandan22781310@gmail.com>
2026-09-25 18:05:13 +0530

    Fix product card layout11

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,19 +1,22 @@
 const multer = require("multer");
-const CloudinaryStorage = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 const cloudinary = require("../config/cloudinary");
 
 const imageStorage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary,
   params: {
     folder: "images",
     resource_type: "image",
-    allowed_formats: ["jpg", "jpeg", "png"],
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
 const uploadImage = multer({
   storage: imageStorage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
 });
 
 module.exports = uploadImage;
